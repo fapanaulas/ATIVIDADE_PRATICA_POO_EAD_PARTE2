a89e1c6f42834119fa5a9a287db2455bd576d45b
lab_dev <lab_dev@ademar-DEV>
2019-10-27 21:57:42 -0400

    conteúdo do projeto

---
+++ src/br/com/fapan/conceitosencapsulamento/Cerveja.java
@@ -0,0 +1,12 @@
+package br.com.fapan.conceitosencapsulamento;
+
+/**
+ *
+ * @author ademar
+ */
+public class Cerveja {
+    private String nome;
+    private String marca;
+    protected String tipoCerveja;
+    public int quantidade = 7;
+}
---
+++ src/br/com/fapan/conceitosencapsulamento/GerenciaCerveja.java
@@ -0,0 +1,18 @@
+package br.com.fapan.conceitosencapsulamento;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ademar
+ */
+public class GerenciaCerveja {
+
+    public static void main(String[] args) {
+         System.out.println("GERÊNCIA DE VENDAS DE CERVEJA");
+         System.out.println("########################################");
+         
+         Cerveja cerva = new Cerveja();
+
+    }
+}
